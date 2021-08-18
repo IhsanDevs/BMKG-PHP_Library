5f0e8c2f6dc79cc86c97eb615033e35856c6a19c
Ihsan Devs <66362682+IhsanDevs@users.noreply.github.com>
2021-08-19 06:46:20 +0700

    Added Demo

--- debug.php
+++ debug.php
@@ -2,8 +2,4 @@
 require __DIR__ . "/src/bmkg.php";
 
 $cuaca = new BMKG;
-// Mendapatkan list Daerah beserta ID Daerah.
-echo json_encode($cuaca->GetListDaerah());
-
-// Mendapatkan Info Cuaca
-echo json_encode($cuaca->GetInfoCuaca(28, "Sulawesi Selatan"));
+echo json_encode($cuaca->GetInfoCuaca(07, "DKI Jakarta"));
